6d51716ef63477243839f7a3180ec512815b8731
Annet7 <orbit-bez-sahara@mail.ru>
2022-11-26 21:01:08 +0700

    решение задачи

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿string[] stringArray = {"bndty", "bn", "156", ",", "%#$", "9"};
+
+string[] group = Array.FindAll(stringArray, max3 => max3.Length <= 3);
+foreach (var max3 in group) Console.WriteLine(max3);
